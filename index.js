8f0ecb4e92756302e15a059474c274e97d590d65
Celestine Kariuki <celestino@safariblocks.com>
2024-02-18 15:12:37 +0000

    put, get all & delete products over HTTP

--- index.js
+++ index.js
@@ -12,16 +12,83 @@ app.get('/', (req, res) => {
     res.send("Hello from Node API Yeah!")
 });
 
-// res -> response
-// req -> request
+
+// POST
 app.post('/api/products', async (req, res) => {
     try{
         const product = await Product.create(req.body);
+        //const products = await Product.find({});
         res.status(200).json(product);
     } catch(error) {
         res.status(500).json({message: error.message});
     }
 });
+
+// Update a product
+app.put('/api/product/:id', async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        // suppose if product doesn't exist
+        if (!product) {
+            return res.status(404).json({message: "Product Not Found"});
+        }
+
+        const updatedProduct = await Product.findById(id);
+
+        res.status(200).json(updatedProduct);
+
+    } catch(error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+// deleting a product
+app.delete('/api/product/:id', async (req, res) => {
+    try {
+
+        const { id, name } = req.params;
+
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({message: "Product Not Found"});
+        }
+
+        res.status(200).json({message: "Product deleted successfully"});
+
+    } catch(error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+
+// res -> response
+// req -> request
+app.get('/api/products', async (req, res) => {
+    try{
+        //const product = await Product.create(req.body);
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch(error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get('/api/product/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch(error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+
 
 mongoose.connect("mongodb+srv://celestino127:C0mpa$$i0n127@cluster0.5qsdpkx.mongodb.net/Simple-Node-API?retryWrites=true&w=majority")
 .then(() => {
