f5ea0415aaff24032df9c410284dc67ad512d09c
Celestine Kariuki <celestino@safariblocks.com>
2024-02-18 12:29:42 +0000

    nodemon mongoose & mongodb added

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.get('/', (req, res) => {
+    res.send("Hello from Node API Yeah!")
 });
 
-app.get('/', (req, res) => {
-    res.send("Hello from Node API")
+mongoose.connect("mongodb+srv://celestino127:C0mpa$$i0n127@cluster0.5qsdpkx.mongodb.net/Simple-Node-API?retryWrites=true&w=majority")
+.then(() => {
+    console.log("connected to the database!");
+    app.listen(3000, () => {
+        console.log('Server running on port 3000');
+    });
+    
 })
-
+.catch(() => {
+    console.log("connection failed!");
+});
 
 
